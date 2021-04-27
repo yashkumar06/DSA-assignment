a1c69319209dac4301061558d1563069fde90023
yashkumar06 <83276235+yashkumar06@users.noreply.github.com>
2021-04-27 22:25:17 +0530

    Add files via upload

---
+++ Question_5.py
@@ -0,0 +1,91 @@
+
+# Python Program to implement BFS and DFS traversal
+
+from collections import defaultdict
+
+class Graph:
+
+    # Constructor
+    def _init_(self):
+
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # function to add an edge to graph
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+
+######################################################################
+    # Function to print a BFS of graph
+    def BFS(self, s):
+
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print (s, end = " ")
+
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+#########################################################################
+
+        # A function used by DFS
+    def DFSUtil(self, v, visited):
+
+        # Mark the current node as visited and print it
+        visited.add(v)
+        print(v, end=' ')
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    # The function to do DFS traversal. It uses
+    # recursive DFSUtil()
+    def DFS(self, v):
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # to print DFS traversal
+        self.DFSUtil(v, visited)
+
+#########################################################################
+
+#creating the graph
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print ("Following is Breadth First Traversal"
+                  " (starting from vertex 2)")
+g.BFS(2)
+
+print("\n\nFollowing is DFS from (starting from vertex 2)")
+g.DFS(2)
+print('\n')
